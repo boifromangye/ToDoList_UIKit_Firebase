fd771c53a9b17b25f4f293886df461d9bff33826
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2026-06-18 16:18:59 +0900

    Create AddToDoViewControllerDelegate.swift

---
+++ ToDoList_UIKit_Firebase/AddToDoViewControllerDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  AddToDoViewControllerDelegate.swift
+//  ToDoList_UIKit_Firebase
+//
+//  Created by 권태우 on 6/18/26.
+//
+
+protocol AddToDoViewControllerDelegate{
+    func onSave(newTask: String)
+}
